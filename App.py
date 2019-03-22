f9850dde72b336346fbeb07ea85cc62c16db7c04
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2019-03-22 23:33:58 +0700

    Add stopword removal

--- App.py
+++ App.py
@@ -1,6 +1,8 @@
 import environ
 from textblob import TextBlob
 from TweetCollector import TweetCollector
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory #untuk menghilangkan stopword /kata sambung
+
 
 
 class App:
@@ -16,14 +18,16 @@ class App:
 
 if __name__ == '__main__':
     app = App()
-
+    factory2 = StopWordRemoverFactory()
+    stopword = factory2.create_stop_word_remover()
     tweetCollector = TweetCollector(app.consumer_key, app.consumer_secret, app.access_token, app.access_secret)
     jokowi = tweetCollector.search_tweets("Jokowi")
     #print(jokowi)
     #print(tweetCollector.search_tweets("Prabowo"))
     for tweet in jokowi:
-        print(tweet.text)
-        analysis = TextBlob(tweet.text)
+        text = stopword.remove(tweet.text) 
+        print(text)
+        analysis = TextBlob(text)
         # analysis = analysis.translate(from_lang='id', to='en')
         print(analysis.sentiment)
         if analysis.sentiment[0]>0:

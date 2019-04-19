ea52ed24e758574e4628c052b2b13fbd2f74946e
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2019-04-19 22:51:45 +0700

    Add method to save datasets

--- TweetCollector.py
+++ TweetCollector.py
@@ -1,3 +1,10 @@
+from Tweet import Tweet
+from TweetCleaner import TweetCleaner
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import \
+    StopWordRemoverFactory
+
+import csv
+import environ
 import tweepy
 
 
@@ -9,3 +16,42 @@ class TweetCollector:
 
     def search_tweets(self, query):
         return self.api.search(query)
+
+if __name__ == '__main__':
+    env = environ.Env(DEBUG=(bool, False), )
+    environ.Env.read_env('.env')
+
+    consumer_key = env("CONSUMER_KEY")
+    consumer_secret = env("CONSUMER_SECRET")
+    access_token = env("ACCESS_TOKEN")
+    access_secret = env("ACCESS_SECRET")
+
+    tweet_cleaner = TweetCleaner()
+    factory2 = StopWordRemoverFactory()
+    stopword = factory2.create_stop_word_remover()
+    tweetCollector = TweetCollector(consumer_key, consumer_secret, access_token, access_secret)
+
+    tweets_jokowi = []
+    tweets_prabowo = []
+
+    jokowi = tweetCollector.search_tweets("Jokowi")
+    prabowo = tweetCollector.search_tweets("Prabowo")
+
+    jokowi = tweetCollector.search_tweets("Jokowi")
+    prabowo = tweetCollector.search_tweets("Prabowo")
+
+    for tweet in jokowi:
+        text = stopword.remove(tweet.text)
+        text = tweet_cleaner.clean(text)
+        tweets_jokowi.append(text)
+
+    for tweet in prabowo:
+        text = stopword.remove(tweet.text)
+        text = tweet_cleaner.clean(text)
+        tweets_prabowo.append(text)
+
+    with open('jokowi_dataset.txt', 'w') as writer:
+        writer.writelines("%s\n" % line for line in tweets_jokowi)
+    
+    with open('prabowo_dataset.txt', 'w') as writer:
+        writer.writelines("%s\n" % line for line in tweets_prabowo)

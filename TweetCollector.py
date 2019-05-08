28ceed6bc15d2dbd3ab31914d3903a79ad999186
Anindito Bhagawanta <anindito.87bagas@gmail.com>
2019-05-08 18:51:19 +0700

    First try getting lots of tweets

--- TweetCollector.py
+++ TweetCollector.py
@@ -1,11 +1,11 @@
 from Tweet import Tweet
 from TweetCleaner import TweetCleaner
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import \
-    StopWordRemoverFactory
+#from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 
 import csv
 import environ
 import tweepy
+import datetime
 
 
 class TweetCollector:
@@ -27,28 +27,39 @@ if __name__ == '__main__':
     access_secret = env("ACCESS_SECRET")
 
     tweet_cleaner = TweetCleaner()
-    factory2 = StopWordRemoverFactory()
-    stopword = factory2.create_stop_word_remover()
+    #factory2 = StopWordRemoverFactory()
+    #stopword = factory2.create_stop_word_remover()
     tweetCollector = TweetCollector(consumer_key, consumer_secret, access_token, access_secret)
 
     tweets_jokowi = []
     tweets_prabowo = []
+    print("Starting...")
+    jokowi = tweetCollector.search_tweets("Jokowi", count=10000)
+    prabowo = tweetCollector.search_tweets("Prabowo", count=10000)
 
-    jokowi = tweetCollector.search_tweets("Jokowi", count=100)
-    prabowo = tweetCollector.search_tweets("Prabowo", count=100)
+    # Mengambil jangka waktu 10 hari sebelum dan setelah pemungutan suara
+    start_date = datetime.datetime(2019, 4, 7)
+    end_date = datetime.datetime(2019, 4, 27)
+    MAX_TWEETS = 1000
 
+    i_jokowi = 0
     for tweet in jokowi:
-        text = stopword.remove(tweet.text)
-        text = tweet_cleaner.clean(text)
-        tweets_jokowi.append(text)
+        # if tweet.created_at < end_date and tweet.created_at > start_date and i_jokowi < MAX_TWEETS:
+        #     tweets_jokowi.append(tweet.text)
+        #     i_jokowi += 1
+        #     if i_jokowi % 100 == 0: print("Getting Jokowi Tweets:", str(i_jokowi), "of", str(MAX_TWEETS))
+        tweets_jokowi.append(tweet.text)
 
+    i_prabowo = 0
     for tweet in prabowo:
-        text = stopword.remove(tweet.text)
-        text = tweet_cleaner.clean(text)
-        tweets_prabowo.append(text)
+        # if tweet.created_at < end_date and tweet.created_at > start_date and i_prabowo < MAX_TWEETS:
+        #     tweets_prabowo.append(tweet.text)
+        #     i_prabowo += 1
+        #     if i_prabowo % 100 == 0: print("Getting Prabowo Tweets:", str(i_prabowo), "of", str(MAX_TWEETS))
+        tweets_prabowo.append(tweet.text)
 
-    with open('jokowi_dataset.txt', 'w') as writer:
+    with open('jokowi_dataset.txt', 'w', encoding="utf-8") as writer:
         writer.writelines("%s\n" % line for line in tweets_jokowi)
     
-    with open('prabowo_dataset.txt', 'w') as writer:
+    with open('prabowo_dataset.txt', 'w', encoding="utf-8") as writer:
         writer.writelines("%s\n" % line for line in tweets_prabowo)
